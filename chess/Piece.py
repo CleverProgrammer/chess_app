9b6ca2f64160889cb4902724135bac2dfd3c4379
Rafeh Qazi <Rafeh@Rafehs-MacBook-Pro.local>
2016-11-04 18:19:20 -0500

    Add capture logic.

--- chess/Piece.py
+++ chess/Piece.py
@@ -14,6 +14,7 @@ square_to_indices_map = {key: (row_index, col_index)
 
 class Piece:
     occupied_squares = {}
+    captured_pieces = []
     pieces = []
     count = 0
     PIECE_VALUES = {
@@ -42,17 +43,31 @@ class Piece:
         raise NotImplementedError
 
     def move(self, square):
-        if square in self.valid_moves():
+        allowed_moves, capture_moves = self.valid_moves()
+        if square in allowed_moves:
             previous_square = self.square
             self.square = square
 
             # update the piece's key in occupied_squares.
             self.occupied_squares[self.square] = self.occupied_squares.pop(previous_square)
 
-            # TODO: Fix line 118 accordingly.
             self.occupied_squares[self.square] = self
+
+        elif square in capture_moves:
+            previous_square = self.square
+            self.square = square
+
+            # update the piece's key in occupied_squares.
+            self.occupied_squares[self.square] = self.occupied_squares.pop(previous_square)
+
+            self.occupied_squares[self.square] = self
+
+
         else:
-            raise AssertionError("Not a valid move.")
+            raise AssertionError(
+                "Not a valid move. Your {} is currently at {} but it is trying to get to {} {}.".format(
+                    self.name, self.square, square)
+            )
 
     def __repr__(self):
         return '\'{}("{}", "{}")\''.format(self.name.title(), self.color, self.square)
@@ -114,16 +129,27 @@ class Knight(Piece):
             eight = None
 
         knight_moves = {one, two, three, four, five, six, seven, eight}
-        allowed_on_board_moves = {move for move in knight_moves if move and move not in self.occupied_squares}
-        return allowed_on_board_moves
+        # allowed_on_board_moves = {move for move in knight_moves if move and move not in self.occupied_squares}
+        allowed = set()
+        capture_moves = set()
+
+        for move in knight_moves:
+            if move:
+                if move not in self.occupied_squares:
+                    allowed.add(move)
+                else:
+                    if not self.occupied_squares[move].color == self.color:
+                        capture_moves.add(move)
+
+        return allowed, capture_moves
 
 
 def simulate_moves(piece):
     moves = []
     play_board = [['-' for _ in range(1, 9)] for _ in range(1, 9)]
-    for _ in range(10):
-        print(piece.valid_moves())
-        piece.move(random.choice(list(piece.valid_moves())))
+    for _ in range(1000):
+        allowed_moves, capture_moves = piece.valid_moves()
+        piece.move(random.choice(list(allowed_moves)))
         print(piece.square)
         moves.append(piece.square)
 
@@ -136,13 +162,23 @@ def simulate_moves(piece):
 
 
 # simulate_moves(Knight('b1'))
-n2 = Knight('b1')
-n3 = Knight('c3')
-print(n2.valid_moves())
-print(n3.valid_moves())
-assert 'b1' not in n3.valid_moves()
-assert 'c3' not in n2.valid_moves()
+# n2 = Knight('b1')
+# n3 = Knight('c3')
+# # print(n2.valid_moves())
+# # print(n3.valid_moves())
+# assert 'b1' not in n3.valid_moves()
+# assert 'c3' not in n2.valid_moves()
+# print(Piece.occupied_squares)
+# n2.move('a3')
+# print(Piece.occupied_squares)
+# # print(n2)
+# # Knight('b1')
+# pprint.pprint(BOARD)
+# n2 = Knight('b1')
+# print(n2.square)
+# print(BOARD[7][7])
+# print(square_to_indices_map)
+n1 = Knight('g1', 'b')
+n2 = Knight('f3')
 print(Piece.occupied_squares)
-n2.move('a3')
-print(n2)
-# Knight('b1')
+print(n1.valid_moves())
