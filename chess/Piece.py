36701a151c0e76c947747403d6e24b831b4f6fbe
Rafeh Qazi <Rafeh@Rafehs-MacBook-Pro.local>
2016-11-04 01:39:17 -0500

    Add all valid knight moves. Ignore .idea/

--- chess/Piece.py
+++ chess/Piece.py
@@ -5,13 +5,14 @@ import pprint
 
 pp = pprint.PrettyPrinter()
 BOARD = [['{}{}'.format(file_, rank) for file_ in 'abcdefgh'] for rank in range(1, 9)]
-coordinate_map = {key: (row_index, col_index)
-                  for row_index, row in enumerate(BOARD) for col_index, key in enumerate(row)}
+square_to_indices_map = {key: (row_index, col_index)
+                         for row_index, row in enumerate(BOARD) for col_index, key in enumerate(row)}
 
-print(coordinate_map)
+print(square_to_indices_map)
 
 
 class Piece:
+    occupied_squares = []
     piece_count = 0
     PIECE_VALUES = {
         'pawn'  : 1,
@@ -22,47 +23,86 @@ class Piece:
         'king'  : 10000
     }
 
-    def __init__(self, name, value, color, position=None):
+    def __init__(self, name, color, square=None):
         self.name = name
-        self.value = value
         self.color = color
-        self.position = position
+        self.square = square
+        self.value = self.PIECE_VALUES[self.name]
         Piece.piece_count += 1
 
     def valid_moves(self):
         raise NotImplementedError
 
     def __repr__(self):
-        return '{} {} {} {}'.format(self.name, self.value, self.color, self.position)
+        return '{} {} {} {}'.format(self.name, self.value, self.color, self.square)
 
 
 class Knight(Piece):
     KNIGHTS = 0
 
-    def __init__(self, color, position):
-        super(Knight, self).__init__('knight', self.PIECE_VALUES['knight'], color, position)
+    def __init__(self, color, square):
+        super(Knight, self).__init__('knight', color, square)
         Knight.KNIGHTS += 1
-        self.file, self.rank = position[0], position[1]
-
+        self.file, self.rank = square[0], square[1]
 
     def valid_moves(self):
-        row, col = coordinate_map[self.position]
-        print(BOARD[row + 2][col + 1])
-        # try:
-        # except IndexError:
-            # print("Your move is off the board")
+        row, col = square_to_indices_map[self.square]
+        try:
+            one = BOARD[abs(row + 2)][abs(col + 1)]
+        except IndexError:
+            one = None
+
+        try:
+            two = BOARD[abs(row + 2)][abs(col - 1)]
+        except IndexError:
+            two = None
+
+        try:
+            three = BOARD[abs(row + 1)][abs(col + 2)]
+        except IndexError:
+            three = None
+
+        try:
+            four = BOARD[abs(row + 1)][abs(col - 2)]
+        except IndexError:
+            four = None
+
+        try:
+            five = BOARD[abs(row - 1)][abs(col - 2)]
+        except IndexError:
+            five = None
+
+        try:
+            six = BOARD[abs(row - 1)][abs(col + 2)]
+        except IndexError:
+            six = None
+
+        try:
+            seven = BOARD[abs(row - 2)][abs(col - 1)]
+        except IndexError:
+            seven = None
+
+        try:
+            eight = BOARD[abs(row - 2)][abs(col + 1)]
+        except IndexError:
+            eight = None
+
+        knight_moves = {one, two, three, four, five, six, seven, eight}
+        allowed_on_board_moves = {move for move in knight_moves if move is not None}
+        return allowed_on_board_moves
 
     def move(self, square):
-        row, col = coordinate_map[square]
+        row, col = square_to_indices_map[square]
         BOARD[row][col] = 'N'
+
 
 # file_rank_to_indices
 
-knight = Knight('w', 'g1')
-knight2 = Knight('w', 'b1')
+knight = Knight('w', 'e4')
+# knight2 = Knight('w', 'b1')
 print(knight)
 print(knight.file, knight.rank)
-print(knight2)
+# print(knight2)
 
 print(Piece.piece_count)
 print(Knight.KNIGHTS)
@@ -70,7 +110,11 @@ print(Knight.KNIGHTS)
 pp.pprint(BOARD)
 # print(BOARD[0].index('g1'))
 print(knight.valid_moves())
-knight.move('f3')
-knight.move('g5')
-pprint.pprint(BOARD[::-1])
+# knight.move('f3')
+# knight.move('g5')
 # print(BOARD[row][col])
+row, col = square_to_indices_map['e4']
+# BOARD[row][col] = 'N'
+pprint.pprint(BOARD[::-1])
+pprint.pprint(knight.valid_moves())
+print(knight.value)
