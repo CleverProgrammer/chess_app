cae6cd1a87698149f74674682f4110206470986a
Rafeh Qazi <Rafeh@10-5-211-241.vlan876.client.wireless.uic.edu>
2016-11-04 11:38:41 -0500

    Bugfix: Allow assertions to let row or column to be zero. Abstract simulate knight moves to simulate piece moves.

--- chess/Piece.py
+++ chess/Piece.py
@@ -25,10 +25,12 @@ class Piece:
 
     def __init__(self, name, square, color='w'):
         self.name = name
-        self.color = color
         self.square = square
+        self.color = color
+
         self.value = self.PIECE_VALUES[self.name]
         self.id = '{}{}{}{}{}'.format(self.name[0], self.color, self.square, self.__class__.count, Piece.count)
+        self.file, self.rank = square
         self.occupied_squares[self.id] = self.square
 
         Piece.pieces.append(self)
@@ -53,7 +55,6 @@ class Knight(Piece):
     def __init__(self, square, color='w'):
         super(Knight, self).__init__('knight', square, color)
         Knight.count += 1
-        self.file, self.rank = square[0], square[1]
 
     def valid_moves(self):
         row, col = square_to_indices_map[self.square]
@@ -63,7 +64,7 @@ class Knight(Piece):
             one = None
 
         try:
-            assert col - 1 > 0
+            assert col - 1 >= 0
             two = BOARD[row + 2][col - 1]
         except (IndexError, AssertionError):
             two = None
@@ -74,31 +75,31 @@ class Knight(Piece):
             three = None
 
         try:
-            assert col - 2 > 0
+            assert col - 2 >= 0
             four = BOARD[row + 1][col - 2]
         except (IndexError, AssertionError):
             four = None
 
         try:
-            assert row - 1 > 0 and col - 2 > 0
+            assert row - 1 >= 0 and col - 2 >= 0
             five = BOARD[row - 1][col - 2]
         except (IndexError, AssertionError):
             five = None
 
         try:
-            assert row - 1 > 0
+            assert row - 1 >= 0
             six = BOARD[row - 1][col + 2]
         except (IndexError, AssertionError):
             six = None
 
         try:
-            assert row - 2 > 0 and col - 1 > 0
+            assert row - 2 >= 0 and col - 1 >= 0
             seven = BOARD[row - 2][col - 1]
         except (IndexError, AssertionError):
             seven = None
 
         try:
-            assert row - 2 > 0
+            assert row - 2 >= 0
             eight = BOARD[row - 2][col + 1]
         except (IndexError, AssertionError):
             eight = None
@@ -108,7 +109,6 @@ class Knight(Piece):
         return allowed_on_board_moves
 
     def move(self, square):
-        row, col = square_to_indices_map[square]
         if square in self.valid_moves():
             self.square = square
 
@@ -121,19 +121,21 @@ class Knight(Piece):
             print("Not a valid move.")
 
 
-n = Knight('g1')
-play_board = [[0 for i in range(1, 9)] for k in range(1, 9)]
-moves = []
-for _ in range(20):
-    print(n.valid_moves())
-    n.move(random.choice(list(n.valid_moves())))
-    print(n.square)
-    moves.append(n.square)
+def simulate_moves(piece):
+    moves = []
+    play_board = [['-' for _ in range(1, 9)] for _ in range(1, 9)]
+    for _ in range(10):
+        print(piece.valid_moves())
+        piece.move(random.choice(list(piece.valid_moves())))
+        print(piece.square)
+        moves.append(piece.square)
 
-print(moves)
-for i, move in enumerate(moves):
-    row, col = square_to_indices_map[move]
-    play_board[row][col] = '{}N'.format(i+1)
+    print(moves)
+    for i, move in enumerate(moves):
+        row, col = square_to_indices_map[move]
+        play_board[row][col] = '{}{}'.format(i + 1, piece.name[:2])
 
-pprint.pprint(play_board[::-1])
-n2 = Knight('b5')
+    pprint.pprint(play_board[::-1])
+
+
+simulate_moves(Knight('b1'))
