8bc5a24ba95121dbf424d03a590c8deadb89e19a
Rafeh Qazi <rafehqazi1@gmail.com>
2016-11-04 23:34:28 -0500

    Add more tests.

--- chess/test_chess.py
+++ chess/test_chess.py
@@ -1,13 +1,19 @@
 import unittest
-from chess import Piece
-from chess import Knight
+from chess import Piece, Knight
 
 
-class TestFifteen(unittest.TestCase):
+class TestChess(unittest.TestCase):
     def test_knight(self):
         knight1 = Knight('b1')
+        moves, captures = knight1.valid_moves()
         self.assertEqual(knight1.color, 'w')
-        # self.assertEqual(fifteen.lower_row_invariant(2, 1), True)
+        self.assertTrue('b1' in Piece.occupied_squares)
+        self.assertEqual({'a3', 'c3', 'd2'}, moves)
+        self.assertEqual(set(), captures)
+
+        # Let's add in another knight.
+        knight2 = Knight('d2', 'b')
+        self.assertEqual(({'a3', 'c3'}, {'d2'}), knight1.valid_moves())
 
 
 if __name__ == '__main__':
